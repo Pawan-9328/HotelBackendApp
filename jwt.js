9e772f55f8cf95a5a3bd46b0c9b3f29dbe47cb68
Pawan Gola <pawangola908@gmail.com>
2024-03-30 00:28:03 +0530

    .env updated

--- jwt.js
+++ jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const jwtAuthMiddleware = (req, res, next) => {
    // first check request headers has authorization or not 
-   const authorization = req.headers.authorization; u
+   const authorization = req.headers.authorization;
    if (!authorization) return res.status(401).json({ error: 'Token Not Found' });
    //..Extract the jwt token from the request headers....   
    const token = req.headers.authorization.split('')[1];
